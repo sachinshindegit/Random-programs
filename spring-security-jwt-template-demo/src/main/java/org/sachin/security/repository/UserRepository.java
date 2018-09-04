1ade1d4455948db21958e41965876579118a3284
sachin.shinde1 <Sachin.Shinde1@veritas.com>
2018-09-04 16:21:02 -0700

    new

---
+++ spring-security-jwt-template-demo/src/main/java/org/sachin/security/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.sachin.security.repository;
+
+import org.sachin.model.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
